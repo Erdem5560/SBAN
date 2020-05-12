02fb7d8d870dff1da0c470ff0ea8ebb7b0c72e35
Erdem5560 <61291735+Erdem5560@users.noreply.github.com>
2020-05-12 21:24:18 +0300

    Create Dropdown.php

---
+++ src/Erdem5560/SureliBan/Form/Element/Dropdown.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+      _____         _                     
+     | ____|_ __ __| | ___ _ __ ___       
+     |  _| | '__/ _` |/ _ \ '_ ` _ \      
+     | |___| | | (_| |  __/ | | | | |     
+     |_____|_|  \__,_|\___|_| |_| |_|     
+          ____ ____   __    ___           
+          | ___| ___| / /_  / _ \         
+          |___ \___ \| '_ \| | | |        
+           ___) |__) | (_) | |_| |        
+          |____/____/ \___/ \___/         
+           ____    _    _   _ 
+          | __ )  / \  | \ | |
+          |  _ \ / _ \ |  \| |
+          | |_) / ___ \| |\  |
+          |____/_/   \_\_| \_|
+                     
+                                          */
+
+declare(strict_types=1);
+
+namespace Erdem5560\SureliBan\Form\Element;
+
+class Dropdown extends BaseSelector{
+
+	public function getType() : string{
+	return "dropdown";
+	}
+
+	protected function serializeElementData() : array{
+	return [
+	"options" => $this->options,
+	"default" => $this->defaultOptionIndex
+	  ];
+	}
+  }
+
