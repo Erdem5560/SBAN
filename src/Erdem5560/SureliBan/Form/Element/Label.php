48ee2cd4db821adfd2c1987b5068225a5ac879d5
Erdem5560 <61291735+Erdem5560@users.noreply.github.com>
2020-05-12 21:25:18 +0300

    Create Label.php

---
+++ src/Erdem5560/SureliBan/Form/Element/Label.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+      _____         _                     
+     | ____|_ __ __| | ___ _ __ ___       
+     |  _| | '__/ _` |/ _ \ '_ ` _ \      
+     | |___| | | (_| |  __/ | | | | |     
+     |_____|_|  \__,_|\___|_| |_| |_|     
+          ____ ____   __    ___           
+          | ___| ___| / /_  / _ \         
+          |___ \___ \| '_ \| | | |        
+           ___) |__) | (_) | |_| |        
+          |____/____/ \___/ \___/         
+           ____    _    _   _ 
+          | __ )  / \  | \ | |
+          |  _ \ / _ \ |  \| |
+          | |_) / ___ \| |\  |
+          |____/_/   \_\_| \_|
+                     
+                                          */
+
+declare(strict_types=1);
+
+namespace Erdem5560\SureliBan\Form\Element;
+
+class Label extends CustomFormElement{
+
+	public function getType() : string{
+	return "label";
+	}
+
+	public function validateValue($value) : void{
+	assert($value === null);
+	}
+
+	protected function serializeElementData() : array{
+	return [];
+	}
+  }
